f3ab4baa47bd57070cc05277402de3dded605e23
Viniski <799048283mw@gmail.com>
2022-12-17 14:45:48 +0100

    Fix a CORS

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://realmadrytbyviniski-api.onrender.com');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header("Access-Control-Allow-Headers", 'content-type, authorization, authorizationalternative, access-control-allow-credentials');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
